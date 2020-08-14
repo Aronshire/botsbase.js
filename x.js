3f6de29a9a0474a5f95c4f6bee7c09d38c49d7cc
Aronshire <64329332+Aronshire@users.noreply.github.com>
2020-08-14 20:49:15 +0300

    Update x.js

--- x.js
+++ x.js
@@ -2,9 +2,36 @@ const request = require('node-superfetch')
 const superagent = require('superagent');
 const { EventEmitter } = require('events')
 
-class BotsBase extends EventEmitter {
-
+class BotsBase {
+    constructor(token) {
+        this.anahtar = token;
+        this.postStats = this.constructor.postStats;
+    };
 // POST SOON
+
+    static async postStats(sunucusayısı, uyesayısı) {
+        if(!this.anahtar) throw new Error("[BotsBase] Veuillez saisir le token.")
+        if(!sunucusayısı) throw new Error("[BotsBase] Un problème est survenu")
+        if(!uyesayısı) throw new Error("[BotsBase] Un problème est survenu")
+        const { body } = await request.get(`https://botsbase.net/api/token/${this.anahtar}`);
+        if(body.message === false) { 
+        throw new Error("[BotsBase] Entrez un token valide.") 
+        return }
+        try{
+           var rewq = require("request");
+           rewq({
+            url: `https://botsbase.net/api/post`,
+            method: "POST",
+              headers: {
+                token: this.anahtar,
+                sunucu_sayisi: sunucusayısı,
+                uye_sayisi: uyesayısı, 
+              } 
+            },(e,r,b) => console.log("Guild & User count posted."));
+        } catch(err) {
+            return console.log(err)
+        }
+    }
 
     async voted(userID, botID) {
         if(!botID) throw new Error(`[BotsBase] Veuillez entrer l’ID du bot.`)
@@ -14,7 +41,7 @@ class BotsBase extends EventEmitter {
             if (!body) return this.emit('error', "Une erreur inconnue a été émise, veuillez en informer les autorités.")
             return body.message
         } catch (err) {
-            return this.emit('error', "Une erreur inconnue a été émise, veuillez en informer les autorités.")
+            return console.log("Une erreur inconnue a été émise, veuillez en informer les autorités.")
         }
     }
 
@@ -25,7 +52,7 @@ class BotsBase extends EventEmitter {
             const { body } = await request.get(`https://botsbase.net/api?bot=${botID}`)
             return body
         } catch(err) {
-            return this.emit('error', "Une erreur inconnue a été émise, veuillez en informer les autorités.")
+            return console.log("Une erreur inconnue a été émise, veuillez en informer les autorités.")
         }
     }
 
@@ -36,9 +63,20 @@ class BotsBase extends EventEmitter {
             const { body } = await request.get(`https://botsbase.net/api/lastvote?id=${botID}`)
             return body.user
         } catch(err) {
-            return this.emit('error', "Une erreur inconnue a été émise, veuillez en informer les autorités.")
+            return console.log("Une erreur inconnue a été émise, veuillez en informer les autorités.")
         }
-    }    
+    } 
+    async lastVote(botID) {
+        if(!botID) throw new TypeError("[BotsBase] Veuillez entrer l’ID du bot.")
+
+        try {
+            const { body } = await request.get(`https://botsbase.net/api/lastvote?id=${botID}`)
+            return body.user
+        } catch(err) {
+            return console.log("Une erreur inconnue a été émise, veuillez en informer les autorités.")
+        }
+    } 
+	
 
 }
 
